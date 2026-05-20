0c20adfe01d7945d303f80c3ce2fe3d1a205a697
s-softech <shaiksiddhik77@gmail.com>
2026-05-20 13:08:24 +0530

    firstCommit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	public static void main(String[] args) throws ClassNotFoundException {
+		System.out.println("main method started from st2 class");
+
+		Class c = Class.forName("com.javaintro.st1");
+		System.out.println(c);
+		Class c2 = Class.forName("java.lang.String");
+		System.out.println(c2);
+		Class c1 = Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println(c1);
+		System.out.println("main methed ended");
+	}
+}
---
+++ src/com/javaintro/st1.java
@@ -0,0 +1,4 @@
+package com.javaintro;
+public class st1{
+	
+}
